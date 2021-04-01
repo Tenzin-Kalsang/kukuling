447609bc59464067eb12669b2737af53bf79d7b0
Tenzin <tznklsng28@gmail.com>
2021-03-31 23:31:18 -0400

    some changes

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -3,28 +3,28 @@ function findAccountById(accounts, id) {
 
 
 function sortAccountsByLastName(accounts) {
-  const sortedNames = accounts.sort((lastA, lastB) =>
-  lastA.name.last.toLowerCase() > lastB.name.last.toLowerCase() ? 1 : -1
-  
-  ); return sortedNames; }
+const sortedNames = accounts.sort((lastA, lastB) =>
+lastA.name.last.toLowerCase() > lastB.name.last.toLowerCase() ? 1 : -1
+); return sortedNames; }
 
 
 function numberOfBorrows(account, books) {
-  let count = 0;
-  books.map(book => book.borrows.map(borrow => { if(borrow.id == account.id) { count += 1 } }))
-  return count;
+let count = 0;
+books.map(book => book.borrows.map(borrow => { if(borrow.id == account.id) { count += 1 } }))
+return count;
 }
 
 function getBooksPossessedByAccount(account, books, authors) {
- let result = books.reduce((acc, book)=>{
-   for (let i=0; i < book.borrows.length; i++){
-     if(book.borrows[i].id === account.id && book.borrows[i].returned === false){acc.push(book)}return acc;
-   }
+let result = books.reduce((acc, book)=>{
+for (let i=0; i < book.borrows.length; i++){
+if(book.borrows[i].id === account.id && book.borrows[i].returned === false){acc.push(book)}return acc;
+}
  },[]);
 
  let author = addAuthor(authors, result);
  return author;
 }
+
 function addAuthor(authors,books){
   for(let i= 0; i < books.length; i++){
     for (let j=0; j < authors.length; j++){
